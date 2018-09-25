b5f2426ee4dc106d98338aae1a33ae8532b04b2e
Nicolás Gomez <nicolasgomezespejo@gmail.com>
2018-09-24 22:53:18 -0300

    :tada: Incremento 2

---
+++ Laboratorio_8/Area.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_8
+{
+    public class Area : Division
+    {
+        public List<Departamento> departamentos;
+
+        public Area(string nombre) : base(nombre)
+        {
+            departamentos = new List<Departamento>();
+        }
+    }
+}
